34d4842b06eb6b9bb36d1dce075294da18a19bad
Elkeid-me <elkeid.me@gmail.com>
2023-10-02 22:03:41 +0800

    Add more tools!

--- src/tools.cxx
+++ src/tools.cxx
@@ -1,1 +1,73 @@
 #include "tools.hxx"
+#include "file_process.hxx"
+#include <arpa/inet.h>
+
+myftp_head::myftp_head(MYFTP_HEAD_TYPE type, unsigned char status,
+                       std::uint32_t length_host_endian)
+    : m_protocol{'\xc1', '\xa1', '\x10', 'f', 't', 'p'}, m_type{type},
+      m_status{status}, m_length{htonl(length_host_endian)}
+{
+}
+
+bool myftp_head::is_valid() const
+{
+    std::string_view protocol{m_protocol, 6};
+    if (protocol != MYFTP_PROTOCOL)
+        return false;
+
+    switch (m_type)
+    {
+    case MYFTP_HEAD_TYPE::OPEN_CONNECTION_REQUEST:
+    case MYFTP_HEAD_TYPE::LIST_REQUEST:
+    case MYFTP_HEAD_TYPE::PUT_REPLY:
+    case MYFTP_HEAD_TYPE::QUIT_REQUEST:
+    case MYFTP_HEAD_TYPE::QUIT_REPLY:
+        if (get_length() != 12)
+            return false;
+        break;
+
+    case MYFTP_HEAD_TYPE::OPEN_CONNECTION_REPLY:
+        if (get_status() != 1 || get_length() != 12)
+            return false;
+        break;
+
+    case MYFTP_HEAD_TYPE::LIST_REPLY:
+    case MYFTP_HEAD_TYPE::GET_REQUEST:
+    case MYFTP_HEAD_TYPE::PUT_REQUEST:
+    case MYFTP_HEAD_TYPE::SHA_REQUEST:
+        if (get_length() <= 13)
+            return false;
+        break;
+
+    case MYFTP_HEAD_TYPE::GET_REPLY:
+    case MYFTP_HEAD_TYPE::SHA_REPLY:
+        if (get_status() != 0 && get_status() != 1 || get_length() != 12)
+            return false;
+
+    case MYFTP_HEAD_TYPE::FILE_DATA:
+        if (get_length() < 12)
+            return false;
+        break;
+
+    default:
+        return false;
+    }
+
+    return true;
+}
+
+MYFTP_HEAD_TYPE myftp_head::get_type() const { return m_type; }
+unsigned char myftp_head::get_status() const { return m_status; }
+std::uint32_t myftp_head::get_length() const { return ntohl(m_length); }
+std::uint32_t myftp_head::get_file_length() const { return get_length() - 12; }
+std::uint32_t myftp_head::get_file_name_length() const { return get_length() - 13; }
+
+void myftp_head::get(int fd_to_host)
+{
+    Read(fd_to_host, reinterpret_cast<char *>(this), 12);
+}
+
+void myftp_head::send(int fd_to_host) const
+{
+    Write(fd_to_host, reinterpret_cast<const char *>(this), 12);
+}
--- src/tools.hxx
+++ src/tools.hxx
@@ -1,24 +1,63 @@
 #ifndef TOOLS_HXX
 #define TOOLS_HXX
 
+#include <cstddef>
 #include <cstdint>
+#include <string_view>
 
 constexpr uint64_t MAGIC_NUMBER_LENGTH{6};
 
-struct [[gnu::packed]] myftp_head
+constexpr std::string_view MYFTP_PROTOCOL{"\xc1\xa1\x10"
+                                        "ftp"};
+
+enum class MYFTP_HEAD_TYPE : unsigned char
 {
-    std::uint8_t m_protocol[MAGIC_NUMBER_LENGTH]; /* protocol magic number (6 bytes) */
-    std::uint8_t m_type;                          /* type (1 byte) */
-    std::uint8_t m_status;                        /* status (1 byte) */
-    std::uint32_t m_length;                       /* length (4 bytes) in Big endian*/
+    OPEN_CONNECTION_REQUEST = 0xa1,
+    OPEN_CONNECTION_REPLY = 0xa2,
+
+    LIST_REQUEST = 0xa3,
+    LIST_REPLY = 0xa4,
+
+    GET_REQUEST = 0xa5,
+    GET_REPLY = 0xa6,
+
+    PUT_REQUEST = 0xa7,
+    PUT_REPLY = 0xa8,
+
+    SHA_REQUEST = 0xa9,
+    SHA_REPLY = 0xaa,
+
+    QUIT_REQUEST = 0xab,
+    QUIT_REPLY = 0xac,
+
+    FILE_DATA = 0xFF,
 };
 
-// enum status
-// {
-//     idle,
-//     open_connection,
-//     main,
-//     end
-// };
+class [[gnu::packed]] myftp_head
+{
+private:
+    char m_protocol[MAGIC_NUMBER_LENGTH];
+    MYFTP_HEAD_TYPE m_type;
+    unsigned char m_status;
+    std::uint32_t m_length;
+
+public:
+    myftp_head(MYFTP_HEAD_TYPE type, unsigned char status,
+               std::uint32_t length_host_endian);
+
+    bool is_valid() const;
+
+    MYFTP_HEAD_TYPE get_type() const;
+    unsigned char get_status() const;
+    std::uint32_t get_length() const;
+    std::uint32_t get_file_length() const;
+    std::uint32_t get_file_name_length() const;
+
+    void get(int fd_to_host);
+    void send(int fd_to_host) const;
+};
+
+const myftp_head
+    OPEN_CONNECTION_REPLY_HEAD(MYFTP_HEAD_TYPE::OPEN_CONNECTION_REPLY, 1, 12);
 
 #endif
