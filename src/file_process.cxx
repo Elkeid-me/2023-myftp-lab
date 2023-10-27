b90918271dd3d2385887baf117837df54fcd7045
Elkeid-me <elkeid.me@gmail.com>
2023-10-27 19:26:18 +0800

    移除无用代码

--- src/file_process.cxx
+++ src/file_process.cxx
@@ -26,46 +26,4 @@ namespace file_process
             error_handle::unix_error("Function `write' error");
         return ret;
     }
-
-    // ssize_t write(int fd, const char *buf, std::size_t n)
-    // {
-    //     std::size_t n_left{n};
-    //     ssize_t n_written;
-
-    //     while (n_left > 0)
-    //     {
-    //         if ((n_written = ::write(fd, buf, n_left)) <= 0)
-    //         {
-    //             if (errno == EINTR)
-    //                 n_written = 0;
-    //             else
-    //                 return -1;
-    //         }
-    //         n_left -= n_written;
-    //         buf += n_written;
-    //     }
-    //     return n;
-    // }
-
-    // ssize_t read(int fd, char *buf, std::size_t n)
-    // {
-    //     std::size_t n_left{n};
-    //     ssize_t nread;
-
-    //     while (n_left > 0)
-    //     {
-    //         if ((nread = ::read(fd, buf, n_left)) < 0)
-    //         {
-    //             if (errno == EINTR)
-    //                 nread = 0;
-    //             else
-    //                 return -1;
-    //         }
-    //         else if (nread == 0)
-    //             break;
-    //         n_left -= nread;
-    //         buf += nread;
-    //     }
-    //     return n - n_left;
-    // }
 }
